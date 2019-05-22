2f79cf55122b7cf89d5d1d17534704db944d73b4
kd.shi <kd.shi@slancer.com>
2019-05-22 09:37:02 +0800

    init

---
+++ cmd/test/demo.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/spfl3/cobra"
+)
+
+func RunServer(stopCh <-chan struct{}) error {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "welcome to my website")
+	})
+	fs := http.FileServer(http.Dir("static/"))
+	http.Handle("/static/", http.StripPrefix("/static/", fs))
+	go func() {
+		http.ListenAndServe(":80", nil)
+	}()
+	<-stopCh
+	<-stopCh
+	return nil
+}
+
+func ServerCommand(stopCh <-chan struct{}) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:  "demo",
+		Long: "just for demo",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return RunServer(stopCh)
+		},
+	}
+
+	return cmd
+}
+
+func main() {
+	cmd = ServerCommand(SetupSignalHandler())
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprint(os.Stderr, "error: %v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ cmd/test/signal.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"os"
+	"os/signal"
+)
+
+var onlyOneSignalHandler = make(chan struct{})
+var shutdownHandler chan os.Signal
+var shutdownSignals = []os.Signal{os.Interrupt}
+
+func SetupSignalHandler() <-chan struct{} {
+	close(onlyOneSignalHandler)
+
+	shutdownHandler = make(chan os.Signal, 2)
+
+	stop := make(chan struct{})
+	signal.Notify(shutdownHandler, shutdownSignals...)
+	go func() {
+		<-shutdownHandler
+		close(stop)
+		<-shutdownHandler
+		os.Exit(1)
+	}()
+	return stop
+
+}
